9ea7763b26ea749968fa446fbaf04e76f1423991
EA <emashliles@gmail.com>
2017-03-29 13:47:31 +0100

    Tidy formatting.

--- test/tic_tac_toe/board_printing_test.clj
+++ test/tic_tac_toe/board_printing_test.clj
@@ -1,8 +1,7 @@
 (ns tic-tac-toe.board-printing-test
   (:require [clojure.test :refer :all]
             [tic-tac-toe.board-printing :refer :all]
-[tic-tac-toe.board :refer :all]
-))
+[tic-tac-toe.board :refer :all]))
 
 (deftest format-board-for-printing
  (is (= "123\n456\n789" (format-board (create-board)))))
