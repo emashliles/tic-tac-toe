c83d08126d4de74100ba9675bb5d706a8f86b823
EA <emashliles@gmail.com>
2017-03-30 16:42:21 +0100

    Add win check for rows.

--- src/tic_tac_toe/board_evaluation.clj
+++ src/tic_tac_toe/board_evaluation.clj
@@ -1,6 +1,28 @@
 (ns tic-tac-toe.board-evaluation)
 
+(defn row-is-win?
+[row]
+(= 1  (count  (into #{} row)))
+)
+
+(defn get-row-sets [rows]
+(some row-is-win? rows)
+
+)
+
+(defn winning-row? [board]
+(partition 3 (vals board))
+)
+
 (defn is-tie? [board]
 (if (= 2 (count (set (vals board))))
 true
 false))
+
+(defn is-win? [board]
+(if (winning-row? board)
+true
+false
+))
+
+
--- test/tic_tac_toe/board_evaluation_test.clj
+++ test/tic_tac_toe/board_evaluation_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [tic-tac-toe.board-evaluation :refer :all]))
 
-(deftest identifies-tie (is (is-tie?  {:0 "O" :1 "X" :2 "O" :3 "X" :4 "O" :5 "X" :6 "O" :7 "X" :8 "O"}  ) ))
+(deftest identifies-tie (is (is-tie?  (sorted-map :0 "O", :1 "X", :2 "O", :3 "X", :4 "O", :5 "X", :6 "O", :7 "X", :8 "O")  ) ))
 
-(deftest identifies-non-tie (is (not (is-tie?  {:0 "" :1 "X" :2 "O" :3 "X" :4 "" :5 "X" :6 "O" :7 "X" :8 "O"}  )) ))
+(deftest identifies-non-tie (is (not (is-tie? (sorted-map :0 "O", :1 "X", :2 "O", :3 "X", :4 "O", :5 "X", :6 "O", :7 "", :8 "O") 
+))))
+
+(deftest identifies-winning-row  (is  (is-win?  (sorted-map :0 "O", :1 "X", :2 "O", :3 "X", :4 "O", :5 "X", :6 "O", :7 "O", :8 "O"))))
