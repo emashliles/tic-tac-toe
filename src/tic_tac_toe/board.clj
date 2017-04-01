b0e4cda0e68f095b3d4956629360248e7ff7c5d1
EA <emashliles@gmail.com>
2017-04-02 00:25:46 +0100

    Rename and format.

--- src/tic_tac_toe/board.clj
+++ src/tic_tac_toe/board.clj
@@ -1,7 +1,8 @@
 (ns tic-tac-toe.board)
 
 (defn create-board []
- (sorted-map :0 "", :1 "", :2 "", :3 "", :4 "", :5 "", :6 "", :7 "", :8 "")) 
+  (sorted-map :0 "", :1 "", :2 "", :3 "", :4 "", :5 "", :6 "", :7 "", :8 "")) 
 
 (defn place-marker [space marker board]
   (assoc board space marker))
+
--- src/tic_tac_toe/board_evaluation.clj
+++ src/tic_tac_toe/board_evaluation.clj
@@ -1,38 +1,34 @@
 (ns tic-tac-toe.board-evaluation)
 
-(defn row-is-win?
-[source-row]
-(let [row (into #{} source-row)]
-(cond (contains? row  "") false
-      (= 1 (count row)) true
-:else false)))
+(defn winning-line? [source-line]
+  (let [line (into #{} source-line)]
+   (cond (contains? line  "") false
+         (= 1 (count line)) true
+         :else false)))
 
-(defn get-row-sets [rows]
-(some row-is-win? rows))
+(defn evaluate-lines [lines] (some winning-line? lines))
+
+(defn separate-lines [board] (partition 3 (vals board)))
 
 (defn winning-row? [board]
-(partition 3 (vals board)))
+  (separate-lines board))
 
 (defn winning-column? [board]
-(winning-row? (select-keys board [:0 :3 :6 :1 :4 :7 :2 :5 :8]) )
-)
+  (separate-lines (select-keys board [:0 :3 :6 :1 :4 :7 :2 :5 :8])))
 
-(defn winning-l-r-diagonal [board]
-(row-is-win? (vals (select-keys board [:0 :4 :8]) ) )
-)
+(defn winning-l-r-diagonal? [board]
+  (winning-line? (vals (select-keys board [:0 :4 :8]))))
 
-(defn winning-r-l-diagonal [board]
-(row-is-win? (vals (select-keys board [:2 :4 :6]) ) )
-)
+(defn winning-r-l-diagonal? [board]
+  (winning-line? (vals (select-keys board [:2 :4 :6]))))
 
 (defn is-tie? [board]
-(let [marker-set (set (vals board)) ]
-(if (contains? marker-set "") false
-true
-)))
+  (let [marker-set (set (vals board))]
+   (if (contains? marker-set "") 
+        false
+        true)))
 
 (defn is-win? [board]
- (or (get-row-sets (winning-row? board)) (get-row-sets (winning-column? board)) (winning-l-r-diagonal board) (winning-r-l-diagonal board))
-)
+  (or (evaluate-lines (winning-row? board)) (evaluate-lines (winning-column? board)) (winning-l-r-diagonal? board) (winning-r-l-diagonal? board)))
 
 
