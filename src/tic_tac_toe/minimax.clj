468f19ed9e988459b99d9579bcc767aa22a10600
EA <emashliles@gmail.com>
2017-04-04 00:42:38 +0100

    Return next move correctly.

--- src/tic_tac_toe/minimax.clj
+++ src/tic_tac_toe/minimax.clj
@@ -9,7 +9,7 @@
 
 (defn minimax-body [board current-player move]
   (let [score (calculate-score current-player board) ]
-   (cond (not (nil? score)) (assoc {} move score)
+   (cond (not (nil? score)) (zipmap [move] [score] )
          (nil? score) 
          (do (let [next-player (switch-marker current-player)]
              (let [moves (available-spaces board)]
@@ -18,25 +18,5 @@
 (defn minimax [board current-player]
   (let [moves (available-spaces board)]
   (let [scores (into {} (map #(minimax-body (place-marker % "O" board) "O" %) moves))] 
-  (best-score scores "O")
-)))     
+    (first (keys (doall (take 1 (best-score scores "O"))))))))     
 
-
-
- ;(first (max-key val (map #( assoc {} % (minimax-body (place-marker % "O" board) "O" %)) moves))) ))
-
-; (first (max-key val (map #(minimax-body (place-marker % "O" board) "O" %) moves))))) 
-
-; (into {} (map #(minimax-body (place-marker % "O" board) "O" %) moves))))   
-
-
-;(defn minimax-body [board current-player move]
-;  (let [score (calculate-score current-player board) ]
-;   (cond (not (nil? score)) (assoc {} move score)
-;         (nil? score) 
-;         (do (let [next-player (switch-marker current-player)]
-;             (let [moves (available-spaces board)]
-;                  (cond (= next-player "O") (do (best-score  (map #(minimax-body (place-marker % "O" board) "O" % ) moves) "O" ) )
-;                        (= next-player "X") (do (best-score  (map #(minimax-body (place-marker % "X" board) "X" % ) moves) "X" ) )
-;
-;)))))))
