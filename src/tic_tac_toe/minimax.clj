65152c0ab589968f88b8218cbe81ef54829aee52
EA <emashliles@gmail.com>
2017-04-10 16:09:51 +0100

    Tinker with minimax.

--- src/tic_tac_toe/minimax.clj
+++ src/tic_tac_toe/minimax.clj
@@ -1,6 +1,13 @@
 (ns tic-tac-toe.minimax 
-  (:require [tic-tac-toe.minimax-scoring :refer :all]
+  (:require [tic-tac-toe.board-evaluation :refer :all]
             [tic-tac-toe.board :refer :all]))
+
+(defn calculate-score [player board max-player min-player]
+  (cond 
+   (and (is-win? board) (= max-player player )) 10
+   (and (is-win? board) (= min-player player )) -10
+   (is-tie? board ) 0
+   :else nil))
 
 (defn switch-marker-mm [marker] 
   (if (= marker "X")
@@ -8,19 +15,22 @@
    "X" ))
 
 (defn best-score [scores player max-player min-player]
-(cond (= player max-player) (do (into  {}  (into {} (max scores))))
-      (= player min-player) (do (into  {}  (into {} (min scores))))))
+(cond (= player max-player) (do (into (sorted-map) (max scores)))
+      (= player min-player) (do (into (sorted-map) (min scores)))))
 
 (defn minimax-body [board current-player move max-player min-player]
-  (let [score (calculate-score current-player board) ]
+  (let [score (calculate-score current-player board max-player min-player) ]
    (cond (not (nil? score)) (zipmap [move] [score] )
          (nil? score) 
          (do (let [next-player (switch-marker-mm current-player)]
              (let [moves (available-spaces board)]
-                 (do (best-score  (map #(minimax-body (place-marker % next-player board) next-player % max-player min-player) moves) next-player max-player min-player) ) ))))))
+                  (do (best-score  (map #(minimax-body (place-marker % next-player board) next-player % max-player min-player) moves) next-player max-player min-player) ) ))))))
 
 (defn minimax [board current-player]
   (let [moves (available-spaces board)]
-  (let [scores (into {} (map #(minimax-body (place-marker % current-player board ) current-player %  current-player (switch-marker-mm current-player)) moves))] 
-    (first (keys (doall (take 1 (best-score scores current-player current-player (switch-marker-mm current-player)))))))))     
+  (let [scores (into (sorted-map) (map #(minimax-body (place-marker % current-player board ) current-player %  current-player (switch-marker-mm current-player)) moves))] 
+;  (println board)
+ (println scores)
+ ; (println moves)
+  (first (keys (doall (take 1 (best-score scores current-player current-player (switch-marker-mm current-player)))))))))     
 
--- test/tic_tac_toe/minimax_scoring_test.clj
+++ test/tic_tac_toe/minimax_scoring_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [tic-tac-toe.minimax-scoring :refer :all]))
 
-(deftest score-winning-max-player-10 (is (= 10 (calculate-score "O" (sorted-map :0 "O", :1 "X", :2 "O", :3 "X", :4 "O", :5 "X", :6 "O", :7 "O", :8 "O")))))
+(deftest score-winning-max-player-10 (is (= 10 (calculate-score "O" (sorted-map :0 "O", :1 "X", :2 "O", :3 "X", :4 "O", :5 "X", :6 "O", :7 "O", :8 "O") ))))
 
 (deftest score-winning-min-player-minus-10 (is (= -10 (calculate-score "X" (sorted-map :0 "O", :1 "X", :2 "O", :3 "X", :4 "X", :5 "X", :6 "O", :7 "X", :8 "O")))))
 
