02e297105c60db3440348bead8aa591f501090d7
EA <emashliles@gmail.com>
2017-04-18 10:20:02 +0100

    Remove test output.

--- test/tic_tac_toe/game_test.clj
+++ test/tic_tac_toe/game_test.clj
@@ -6,7 +6,7 @@
 (deftest start-game (is (= "\u001b[2J\u001B[0;0f| 1 || 2 || 3 |\n---------------\n| 4 || 5 || 6 |\n---------------\n| 7 || 8 || 9 |\n" (with-out-str (start)))))
 
 (deftest start-game-returns-board (is 
-  (= {:0 "", :1 "", :2 "", :3 "", :4 "", :5 "", :6 "", :7 "", :8 ""}  (start))))
+  (with-out-str (= {:0 "", :1 "", :2 "", :3 "", :4 "", :5 "", :6 "", :7 "", :8 ""}  (start)))))
 
 (deftest make-human-move (is 
   (= "\u001b[2J\u001B[0;0f| 1 || X || 3 |\n---------------\n| 4 || 5 || 6 |\n---------------\n| 7 || 8 || 9 |\n" (with-out-str (with-in-str "2" (human-turn (create-board) "X" )) ))) )
