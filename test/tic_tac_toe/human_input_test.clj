267acd50d279755dc5790d00fb6c265956343049
EA <emashliles@gmail.com>
2017-04-18 10:16:35 +0100

    Remove test output.

--- test/tic_tac_toe/human_input_test.clj
+++ test/tic_tac_toe/human_input_test.clj
@@ -4,9 +4,9 @@
 
 (deftest get-user-selection (is (= "1" (with-in-str "2" (get-human-selection)) )))
 
-(deftest negative-selection-invalid (is (= "0" (with-in-str "-1\n1\n" (get-human-selection)))))
+(deftest negative-selection-invalid (with-out-str (is (= "0" (with-in-str "-1\n1\n" (get-human-selection))))))
 
-(deftest non-number-invalid (is (= "0" (with-in-str "hello\n1\n" (get-human-selection)))))
+(deftest non-number-invalid (with-out-str (is (= "0" (with-in-str "hello\n1\n" (get-human-selection))))))
 
 (deftest advice-text-for-invalid-input (is (= "Invalid selection. Please enter a number between 1 and 9\n" (with-out-str (with-in-str "invalid\n1\n" (get-human-selection))))))
 
